a3c0493a6cbb6cbfe1d3b9616c994673d82833d7
Cauã Vinicius Gomes Monteiro <cauavinicius.contato@gmail.com>
2024-08-28 23:24:57 -0300

    Update app module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { TaskModule } from './tasks/task.module';
+import { PrismaModule } from 'prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [TaskModule, PrismaModule],
   controllers: [],
   providers: [],
 })
