6f45b62a8b0d4e75d99d046129eed4bd8ec2de2d
Cauã Vinicius Gomes Monteiro <cauavinicius.contato@gmail.com>
2024-08-29 17:47:07 -0300

    Update UserModule with necessary providers and imports

---
+++ backend/src/users/user.module.ts
@@ -0,0 +1,12 @@
+import { AuthModule } from 'src/auth/auth.module';
+import { UserController } from './user.controller';
+import { UserService } from './user.service';
+import { PrismaModule } from 'prisma/prisma.module';
+import { Module } from '@nestjs/common';
+
+@Module({
+  imports: [PrismaModule, AuthModule],
+  controllers: [UserController],
+  providers: [UserService],
+})
+export class UserModule {}
