9cd0e2ae70b8f2b6dc5d630c112b188803b629af
Cauã Vinicius Gomes Monteiro <cauavinicius.contato@gmail.com>
2024-08-31 17:37:58 -0300

    Update authModule

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,8 +1,28 @@
-import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UserModule } from 'src/users/user.module';
+import { Module } from '@nestjs/common';
+import { PrismaModule } from 'prisma/prisma.module';
+import { JwtStrategy } from './JwtStrategy';
+import { AuthController } from './auth.controller';
 
 @Module({
-  providers: [AuthService],
-  exports: [AuthService],
+  imports: [
+    PassportModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '60m' },
+      }),
+      inject: [ConfigService],
+    }),
+    UserModule,
+    PrismaModule,
+  ],
+  providers: [AuthService, JwtStrategy],
+  controllers: [AuthController],
 })
 export class AuthModule {}
