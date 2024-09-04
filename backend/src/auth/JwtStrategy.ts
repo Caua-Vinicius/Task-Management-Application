ec354b68db1b6698100d07d49c6587bc007f9bdf
Cauã Vinicius Gomes Monteiro <cauavinicius.contato@gmail.com>
2024-09-03 21:24:30 -0300

    Update return of validate method

--- backend/src/auth/JwtStrategy.ts
+++ backend/src/auth/JwtStrategy.ts
@@ -24,7 +24,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     }
     return {
       user: {
-        id: user.id,
+        user_id: user.id,
         email: user.email,
       },
     };
